f05ff88c6933b8e427551ba05faba74f24ba713b
Bzzziker <Bzzziker@mail.ru>
2019-05-22 09:00:00 +0300

    asc

---
+++ MyGameProject/Assets/Player/PlayerCharacterContoller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCharacterContoller : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+
+    public Vector3 target_null_character;
+    public bool target_null_character_boll = false;
+    public float target_distance;
+    public float target_distance_stop = 10;
+
+    private void Awake()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void welk(Vector3 target)
+    {
+      
+        agent.SetDestination(target);
+  
+    }
+
+}
---
+++ MyGameProject/Assets/Player/PlayerMauseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMauseController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target_point;
+
+    [SerializeField]
+    GameObject ActivObject;  //Активный объект 
+
+    public int one;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _raycast();
+         
+        }
+
+
+    }
+
+    void _raycast()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+
+        if (hit.collider.tag == null) one++;
+        if((hit.collider.tag=="Characters")||(hit.collider.tag=="Infrastructure"))
+        {
+            ActivObject = hit.collider.gameObject;
+        }
+        
+        //ActivObject.GetComponent<PlayerCharacterContoller>().welk(hit.point);
+    }
+}
